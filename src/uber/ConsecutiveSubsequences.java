5c8e7b8e813924070c60fec0dd0945a8e55f7dc7
rc3097 <rc3097@nyu.edu>
2016-06-22 12:33:14 -0400

    Update to long

--- src/uber/ConsecutiveSubsequences.java
+++ src/uber/ConsecutiveSubsequences.java
@@ -1,25 +1,27 @@
 package uber;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class ConsecutiveSubsequences {
-	public int countSubArray(int[] nums, int k) {
-		HashMap<Integer, ArrayList<Integer>> sumMap = new HashMap();
-		int[] sums = new int[nums.length + 1];
-		ArrayList<Integer> temp = new ArrayList<Integer>();
-		temp.add(1);
+	public long countSubArray(int[] nums, long k) {
+		HashMap<Long, ArrayList<Long>> sumMap = new HashMap();
+		long[] sums = new long[nums.length + 1];
+		ArrayList<Long> temp = new ArrayList<Long>();
+		temp.add((long) 1);
 		sumMap.put(k, temp);
-		int res =0 ;
+		long res =0 ;
 		for (int i = 0; i < nums.length; i++) {
 			sums[i+1] =  sums[i] + nums[i];
-			int mod = sums[i+1] %k;
-			temp = new ArrayList<Integer>();
+			long mod = sums[i+1] %k;
+			temp = new ArrayList<Long>();
 			if (sumMap.containsKey(k-mod)) {
 				temp = sumMap.get(k-mod);
 			} 
 			res += temp.size();
-			temp.add(i+1);
+			temp.add((long) (i+1));
 			sumMap.put(k-mod, temp);
 		}
 		return res;
@@ -27,8 +29,18 @@ public class ConsecutiveSubsequences {
 	
 	public static void main(String[] args) {
 		ConsecutiveSubsequences q = new ConsecutiveSubsequences();
-		System.out.println(q.countSubArray(new int[]{1,2,3,4,1}, 3));
-		System.out.println(q.countSubArray(new int[]{1,2,1,2,1,2}, 2));
-		System.out.println(q.countSubArray(new int[]{}, 2));
+		// System.out.println(q.countSubArray(new int[]{1,2,3,4,1}, 3));
+		// System.out.println(q.countSubArray(new int[]{1,2,1,2,1,2}, 2));
+		// System.out.println(q.countSubArray(new int[]{}, 2));
+
+		Scanner in  = new Scanner(System.in);
+		for (int i =0; i<in.nextInt();i++) {
+			int n = in.nextInt();
+			int k = in.nextInt();
+			int[] nums = new int[n];
+			for (int j =0;j<n;j++) 
+				nums[j] = in.nextInt();
+			System.out.println(q.countSubArray(nums, k));
+		}
 	}
 }
