72a9845c3783fcfc354f42cd071e2b38f33076b4
rc3097 <rc3097@nyu.edu>
2016-07-12 22:32:10 -0400

    insert node

---
+++ src/bloomberg/InsertNodeInaSortedList.java
@@ -0,0 +1,26 @@
+
+package  bloomberg;
+/*
+* insert node  in a sorted list
+*/
+public class InsertNodeInaSortedList {
+	public ListNode insert(ListNode head, int num) {
+		if (head==null) {
+			return new ListNode(num);
+		}
+		ListNode dummyhead =  new ListNode(0);
+		dummyhead.next = head;
+		ListNode pre =  dummyhead;
+		ListNode root =  head;
+		while (root!=null && root.val<num) {
+			pre = root;
+			root = root.next;
+		}
+
+		ListNode numNode = new ListNode(num);
+		ListNode temp = pre.next;
+		pre.next = numNode;
+		numNode.next = temp;
+		return dummyhead.next();
+	}
+}
