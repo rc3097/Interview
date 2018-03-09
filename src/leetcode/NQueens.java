e923097a7062c9cc48f81473a7cea26fb9b92d5d
Runpeng Chen <rc3097@nyu.edu>
2018-03-09 19:48:19 +0000

    add NQueens

--- src/leetcode/NQueens.java
+++ src/leetcode/NQueens.java
@@ -1,71 +1,58 @@
 package leetcode;
+
+import org.bouncycastle.util.Arrays;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class NQueens {
-	public List<List<String>> solveNQueens(int n) {
-		List<List<String>> result = new ArrayList<List<String>>();
-		dfs(result, new ArrayList<String>(), 0, n);
-		return result;
-	}
 
-	private void dfs(List<List<String>> result, ArrayList<String> currentMap,
-			int row, int n) {
-		// TODO Auto-generated method stub
-		if (row == n) {
-			result.add((List<String>) currentMap.clone());
-			return;
-		}
-		// new row
-		char[] nrow = new char[n];
-		for (int i = 0; i < n; i++) {
-			nrow[i]='.';
-		}
+    public List<List<String>> solveNQueens(int n) {
+        ArrayList<List<String>> result = new ArrayList<>();
+        dfs(new boolean[n], new boolean[2 * n - 1], new boolean[2 * n - 1], new ArrayList<>(), result, 0, n);
+        return result;
+    }
 
-		for (int i = 0; i < n; i++) {
-			if (checkqueens(currentMap, row, i, n)) {
-				nrow[i]='Q';
-				currentMap.add(String.valueOf(nrow));
-				dfs(result, currentMap, row+1, n);
-				currentMap.remove(row);
-				nrow[i] ='.';
-			}
-		}
+    private void dfs(boolean[] colCheck, boolean[] diag1Check, boolean[] diag2Check, List<String> currentMap, List<List<String>> result, int row, int n) {
+        if (row == n) {
+            result.add(new ArrayList<>(currentMap));
+            return;
+        }
+        for (int col = 0; col < n; col++) {
+            int diag1 = row + col;
+            int diag2 = row - col + n - 1;
+            if (colCheck[col] || diag1Check[diag1] || diag2Check[diag2]) continue;
+            colCheck[col] = true;
+            diag1Check[diag1] = true;
+            diag2Check[diag2] = true;
+            currentMap.add(generateRow(col, n));
+            dfs(colCheck, diag1Check, diag2Check, currentMap, result, row + 1, n);
+            currentMap.remove(currentMap.size() - 1);
+            colCheck[col] = false;
+            diag1Check[diag1] = false;
+            diag2Check[diag2] = false;
+        }
+    }
 
-	}
+    private String generateRow(int col, int n) {
+        String row = "";
+        for (int i = 0; i < n; i++) {
+            row += i == col ? "Q" : ".";
+        }
+        return row;
+    }
 
-	private boolean checkqueens(ArrayList<String> currentMap, int row,
-			int column, int n) {
-		// TODO Auto-generated method stub
-		// up
-		for (String string : currentMap) {
-			if (string.charAt(column) == 'Q') {
-				return false;
-			}
-		}
 
-		int[] leftup = new int[] { -1, -1 };
-		int[] rightup = new int[] { -1, 1 };
-		int row_i = row - 1;
-		int column_i = column - 1;
-		while (row_i >= 0 && column_i >= 0) {
-			if (currentMap.get(row_i).charAt(column_i) == 'Q') {
-				return false;
-			}
-			row_i += leftup[0];
-			column_i += leftup[1];
-		}
-		row_i = row - 1;
-		column_i = column + 1;
-		while (row_i >= 0 && column_i < n) {
-			if (currentMap.get(row_i).charAt(column_i) == 'Q') {
-				return false;
-			}
-			row_i += rightup[0];
-			column_i += rightup[1];
-		}
+    public static void main(String[] args) {
+        NQueens nQueens = new NQueens();
+        System.out.println( nQueens.solveNQueens(1).size());
+        System.out.println( nQueens.solveNQueens(2).size());
+        System.out.println( nQueens.solveNQueens(3).size());
+        System.out.println( nQueens.solveNQueens(4).size());
+        System.out.println( nQueens.solveNQueens(5).size());
+        System.out.println( nQueens.solveNQueens(6).size());
+        System.out.println( nQueens.solveNQueens(7).size());
+        System.out.println( nQueens.solveNQueens(8).size());
 
-		return true;
-	}
-
+    }
 }
