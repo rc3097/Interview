6a27da83d9a1053f1642a0076e7446b09f253557
rc3097 <rc3097@nyu.edu>
2018-03-09 13:44:42 -0800

    NQueensII

--- src/leetcode/NQueensII.java
+++ src/leetcode/NQueensII.java
@@ -1,72 +1,38 @@
 package leetcode;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class NQueensII {
-	int result=0;
-	public int totalNQueens(int n) {
-		result=0;
-		dfs( new ArrayList<String>(), 0, n);
-		return result;
-	}
+    public int totalNQueens(int n) {
+        return dfs(new boolean[n],new boolean[2*n-1], new boolean[2*n-1],0, n );
+    }
 
-	private void dfs( ArrayList<String> currentMap,
-			int row, int n) {
-		// TODO Auto-generated method stub
-		if (row == n) {
-			result++;
-			return;
-		}
-		// new row
-		char[] nrow = new char[n];
-		for (int i = 0; i < n; i++) {
-			nrow[i]='.';
-		}
+    private int dfs(boolean[] colCheck, boolean[] diag1Check, boolean[] diag2Check, int row, int n) {
+        if (row == n) return 1;
+        int result = 0;
+        for (int col = 0; col < n; col++) {
+            int diag1 = row + col;
+            int diag2 = row - col + n - 1;
+            if (colCheck[col] || diag1Check[diag1] || diag2Check[diag2]) continue;
+			colCheck[col] = true;
+			diag1Check[diag1] = true;
+			diag2Check[diag2] = true;
+            int temp = dfs(colCheck, diag1Check, diag2Check, row + 1, n);
+            result +=temp;
+            colCheck[col] = false;
+			diag1Check[diag1] = false;
+			diag2Check[diag2] =false;
+        }
+        return result;
+    }
 
-		for (int i = 0; i < n; i++) {
-			if (checkqueens(currentMap, row, i, n)) {
-				nrow[i]='Q';
-				currentMap.add(String.valueOf(nrow));
-				dfs(currentMap, row+1, n);
-				currentMap.remove(row);
-				nrow[i] ='.';
-			}
-		}
+    public static void main(String[] args) {
+        NQueensII nQueensII = new NQueensII();
+        for (int i = 1; i < 9 ; i++) {
+            System.out.println( nQueensII.totalNQueens(i));
+        }
+    }
 
-	}
-
-	private boolean checkqueens(ArrayList<String> currentMap, int row,
-			int column, int n) {
-		// TODO Auto-generated method stub
-		// up
-		for (String string : currentMap) {
-			if (string.charAt(column) == 'Q') {
-				return false;
-			}
-		}
-
-		int[] leftup = new int[] { -1, -1 };
-		int[] rightup = new int[] { -1, 1 };
-		int row_i = row - 1;
-		int column_i = column - 1;
-		while (row_i >= 0 && column_i >= 0) {
-			if (currentMap.get(row_i).charAt(column_i) == 'Q') {
-				return false;
-			}
-			row_i += leftup[0];
-			column_i += leftup[1];
-		}
-		row_i = row - 1;
-		column_i = column + 1;
-		while (row_i >= 0 && column_i < n) {
-			if (currentMap.get(row_i).charAt(column_i) == 'Q') {
-				return false;
-			}
-			row_i += rightup[0];
-			column_i += rightup[1];
-		}
-
-		return true;
-	}
 }
